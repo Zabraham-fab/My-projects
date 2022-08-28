61726db38f4f10ff5a0c97a4a9e8990bf250cd34
Zabraham-fab <mf1000gol@gmail.com>
2022-08-28 14:46:41 +0300

    host=0.0.0.0 and activated port=80 in EC2

--- flask-03-04-If-Handling-Routes_Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
+++ flask-03-04-If-Handling-Routes_Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Create welcome page with main.html file and assing it to the root path
 @app.route('/')
 def home():
-    return render_template('main.html', name='Serdar')
+    return render_template('main.html', name='Zabraham')
 
 # Write a function named `greet` which uses template file named `greet.html` given under 
 # `templates` folder. it takes parameters from query string on URL, assign that parameter 
@@ -30,7 +30,7 @@ def login():
     if request.method == 'POST':
         user_name = request.form['username']
         password = request.form['password']
-        if password == 'clarusway':
+        if password == 'zabraham':
             return render_template('secure.html', user=user_name.title())
         else:
             return render_template('login.html', user=user_name.title(), control = True)
@@ -39,5 +39,5 @@ def login():
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=80)
